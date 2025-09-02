94aefb0be45545cccbd90686c839fa83a1ae1002
Fernando José Fuentes Castillo <fernandocastillo.8@hotmail.com>
2025-09-01 19:32:43 -0600

    AAAAAAAAAAAAAAAAA

--- lib/main.dart
+++ lib/main.dart
@@ -25,49 +25,69 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Mi App")),
-      body: Column(
-        children: [
-          HeaderCard(),
-          SizedBox(height: 16),
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade200,
-                    blurRadius: 4,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Experiencia",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: 18,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text("Soy un cargador de huevos"),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Text("Enero 2020 - Presente", style: TextStyle(fontWeight: FontWeight.bold)),
-                  SizedBox(height: 8),
-                  Text("Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-                ],
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderCard(),
+            SizedBox(height: 16),
+            ExperienceCard("Cargador de Huevos", "Soy un cargador de huevos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            ExperienceCard("Cargador de Pollos", "Soy un cargador de pollos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            ExperienceCard("Cargador de Bollos", "Soy un cargador de bollos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            ExperienceCard("Cargador de Leches", "Soy un cargador de leches", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExperienceCard extends StatelessWidget {
+
+  final String title;
+  final String company;
+  final String duration;
+  final String description;
+
+  const ExperienceCard(this.title, this.company, this.duration, this.description, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              blurRadius: 4,
+              offset: Offset(0, 2),
             ),
-          ),
-        ],
+          ],
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Experiencia",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(duration, style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 8),
+            Text(description),
+          ],
+        ),
       ),
     );
   }
