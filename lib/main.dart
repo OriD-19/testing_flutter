472b41ae04c09a5e2fb9adcd050e6d58f0068ea9
Fernando José Fuentes Castillo <fernandocastillo.8@hotmail.com>
2025-09-06 08:26:08 -0600

    ASDFJLASJDF

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,160 @@ class HomeScreen extends StatelessWidget {
           children: [
             HeaderCard(),
             SizedBox(height: 16),
-            ExperienceCard("Cargador de Huevos", "Soy un cargador de huevos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-            ExperienceCard("Cargador de Pollos", "Soy un cargador de pollos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-            ExperienceCard("Cargador de Bollos", "Soy un cargador de bollos", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-            ExperienceCard("Cargador de Leches", "Soy un cargador de leches", "Enero 2020 - Presente", "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            ExperienceCard(
+              "Cargador de Huevos",
+              "Soy un cargador de huevos",
+              "Enero 2020 - Presente",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
+            ExperienceCard(
+              "Cargador de Pollos",
+              "Soy un cargador de pollos",
+              "Enero 2020 - Presente",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
+            ExperienceCard(
+              "Cargador de Bollos",
+              "Soy un cargador de bollos",
+              "Enero 2020 - Presente",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
+            ExperienceCard(
+              "Cargador de Leches",
+              "Soy un cargador de leches",
+              "Enero 2020 - Presente",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
+            Text(
+              "Mi educacion",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            EducationWidget(),
+            Text(
+              "Mis habilidades",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade200,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+                color: Colors.white,
+              ),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  children: [
+                    Chip(label: Text("Flutter")),
+                    SizedBox(width: 8),
+                    Chip(label: Text("Dart")),
+                    SizedBox(width: 8),
+                    Chip(label: Text("Firebase")),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SkillsWidget extends StatelessWidget {
+  const SkillsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Habilidades",
+                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  "Flutter, Dart, Firebase",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EducationWidget extends StatelessWidget {
+  const EducationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Educación",
+                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  "Ingeniería en Sistemas",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text("2015 - 2020", style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 8),
+            Text(
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ),
           ],
         ),
       ),
@@ -42,13 +192,18 @@ class HomeScreen extends StatelessWidget {
 }
 
 class ExperienceCard extends StatelessWidget {
-
   final String title;
   final String company;
   final String duration;
   final String description;
 
-  const ExperienceCard(this.title, this.company, this.duration, this.description, {super.key});
+  const ExperienceCard(
+    this.title,
+    this.company,
+    this.duration,
+    this.description, {
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -73,10 +228,7 @@ class ExperienceCard extends StatelessWidget {
               children: [
                 Text(
                   "Experiencia",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 18,
-                  ),
+                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
                 ),
                 SizedBox(width: 8),
                 Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
